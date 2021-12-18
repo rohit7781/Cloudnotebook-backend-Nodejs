8b716b497e4322f0bc3c5aa648823776e31da299
Rohit Kumar <rohitkumar703314@gmail.com>
2021-12-18 20:50:00 +0530

    added hasing bcrypt

---
+++ routes/auth.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const User = require('../models/User');
+const router = express.Router()
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+
+
+// Create a user using = POTS : /api/auth/createuser
+router.post('/createuser',[
+    body('name','Enter a valid name').isLength({ min: 3 }),
+    body('email','Enter a valid email').isEmail(),
+    body('password','Password must be more than 5  character').isLength({ min: 5 }),
+
+],async (req, res)=>{
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+        
+    
+    let user = await User.findOne({email:req.body.email});
+    if (user) {
+        return res.status(400).json({error:"Email already registered"})
+    }
+
+    const salt = await bcrypt.genSalt(10)
+
+    secPass = await bcrypt.hash(req.body.password,salt) ;
+    user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: secPass,
+      })
+      
+    res.json(user);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("some error occurs")
+    }
+} )
+
+module.exports = router
