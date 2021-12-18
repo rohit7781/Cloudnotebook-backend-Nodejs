cb38e8faa8dbbe00464b906efbfe0469e918f374
Rohit Kumar <rohitkumar703314@gmail.com>
2021-12-18 21:58:35 +0530

    added login route

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,7 @@ const bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
 var token = jwt.sign({ foo: 'bar' }, 'shhhhh');
 
-// Creating my own secret key for verification
+// Creating my own secret key for verification . No login required
 JWT_SECRET = "my$&*@$Rohit&&"
 
 
@@ -52,8 +52,55 @@ router.post('/createuser',[
 
     } catch (error) {
         console.error(error.message);
-        res.status(500).send("some error occurs")
+        res.status(500).send("Internal server error occurs")
     }
 } )
+
+
+
+// Authenticate a user using = POTS : /api/auth/login No login required
+router.post('/login',[
+    body('email','Enter a valid email').isEmail(),
+    body('password','Enter your password please').exists(),
+],async (req, res)=>{
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {email,password} = req.body;
+    try {
+        let user = await User.findOne({email});
+        if (!user) {
+            return res.status(400).json({
+                error: "Sorry user doesn't exits"
+            });
+        }
+
+        const passwordcompare = await bcrypt.compare(password,user.password)
+        if (!passwordcompare) {
+            return res.status(400).json({error:"Enter correct password"});
+        }
+
+        const data = {
+            user :{
+                id : user.id
+            }
+        }
+        const authtoken = jwt.sign(data,JWT_SECRET)
+        res.json(authtoken);
+    
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal server error occurs")
+    }
+    
+} )
+
+
+
+
 
 module.exports = router
