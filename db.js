66b0b136770d64d71785293f3e11cbb90e64b35c
Rohit Kumar <rohitkumar703314@gmail.com>
2022-01-05 21:55:16 +0530

    Connecting to atlas

--- db.js
+++ db.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+// const mongoURI = "mongodb://localhost:27017/inotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+
+const mongoURI = "mongodb+srv://couldnotebook:couldnotebook@cluster0.6zwgk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI, ()=>{
         console.log("Sucess")
